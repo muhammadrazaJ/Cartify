08cc5cbb0e9320c770c481c91805fbfff5d1a076
Muhammad Raza <raza.jessani@yahoo.com>
2026-02-05 17:27:09 +0500

    Initial Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cartify.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
